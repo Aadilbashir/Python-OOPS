03f152a2ab703a4d9434cdb12689802f167e9751
Aadil Bashir <mraadilbashir@gmail.com>
2025-10-25 20:04:52 +0300

    upload

---
+++ Summary/Summary.py
@@ -0,0 +1,26 @@
+from car import Car
+
+
+car1 = Car("Ferrari", 2024, "black", False)
+car2 = Car("BMW", 2026, "Yellow", True)
+car3 = Car("Porsche", 2023, "White", True)
+
+
+car1.drive()
+car1.stop()
+car1.describe()
+
+
+"""
+print(car1.model)
+print(car1.year)
+print(car1.color)
+
+print(car2.model)
+print(car2.year)
+print(car2.color)
+
+print(car3.model)
+print(car3.year)
+print(car3.color)
+"""
